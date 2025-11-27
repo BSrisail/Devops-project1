978bf1bc9ef69ff02c37e5e2d926df01b1e05bd4
srisail <srisail.09.09@gmail.com>
2025-11-27 10:32:33 +0530

    update2

--- web.sh
+++ web.sh
@@ -2,12 +2,13 @@ dnf install nginx -y
 
 rm -rf /usr/share/nginx/html/* 
 
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-cp expense.conf /etc/nginx/default.d/expense.conf
 
 systemctl enable nginx 
 systemctl start nginx 
