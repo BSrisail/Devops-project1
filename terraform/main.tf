68568febe42a6625b75ea3a9acda9c397b3b9cb3
srisail <srisail@mail>
2026-01-20 17:39:51 +0530

    tf4

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
 provider "aws" {
-  region = "us-east-1"  # Change to your preferred region
+  region = "us-east-1" 
 }
 
-resource "aws_instance" "FE" {
+resource "aws_instance" "tf-server" {
   ami                    = "ami-0b4f379183e5706b9" 
-  instance_type          = "t3.micro"              
+  instance_type          = "t2.micro"              
   vpc_security_group_ids = ["sg-0ed5cc77a2216d028"] 
   subnet_id              = "subnet-0b311752c17c09dbb" 
 
   tags = {
-    Name        = "FE"
+    Name        = "tf-server"
     Environment = "Development"
     Terraform   = "true"
   }
